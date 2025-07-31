c074417ce8bbb4605e0d01fb856fb3d2e2acb730
Calacuda <eowest@gmail.com>
2025-07-31 18:42:40 -0400

    backup commit

--- midi-daw-server/src/server/mod.rs
+++ midi-daw-server/src/server/mod.rs
@@ -1,2 +1,5 @@
 // the server should keep track of what notes are being played on each output and should panic then
 // kill all notes on client disconnect.
+
+#[actix::main]
+pub async fn server() {}
