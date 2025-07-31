0955e16269d4ea77d796107b9ac0bd64aba176f6
Calacuda <eowest@gmail.com>
2025-07-31 18:28:22 -0400

    backup commit

---
+++ midi-daw-server/src/midi/automation.rs
@@ -0,0 +1,9 @@
+// use crossbeam::channel::{Receiver, Sender};
+// use midi_msg::MidiMsg;
+//
+// pub fn automation(
+//     new_dev_tx: Receiver<AutomationConf>,
+//     midi_msg_out: Sender<(String, MidiMsg)>,
+// ) -> ! {
+//     loop {}
+// }
