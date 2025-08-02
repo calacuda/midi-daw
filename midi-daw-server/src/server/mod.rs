f27e254f744461f663456fba44b33cb883d71a3b
Calacuda <eowest@gmail.com>
2025-08-02 14:13:59 -0400

    cleaned dead code

--- midi-daw-server/src/server/mod.rs
+++ midi-daw-server/src/server/mod.rs
@@ -27,12 +27,8 @@ async fn midi(
             velocity,
             duration,
         } => {
-            // if let Ok(tempo) = tempo.read() {
             let tempo = tempo.read().await;
             play_note(*tempo, midi_out, dev, channel, note, velocity, duration).await;
-            // } else {
-            // error!("failed to read tempo");
-            // }
         }
         MidiMsg::StopNote { note } => stop_note(midi_out, dev, channel, note).await,
         MidiMsg::CC { control, value } => send_cc(midi_out, dev, channel, control, value).await,
@@ -43,38 +39,20 @@ async fn midi(
 }
 
 #[post("tempo")]
-async fn set_tempo(
-    tempo: web::Data<RwLock<f64>>,
-    // midi_out: web::Data<MidiOut>,
-    req_body: Json<f64>,
-) -> impl Responder {
-    // if let Ok(mut tempo) = tempo.write() {
+async fn set_tempo(tempo: web::Data<RwLock<f64>>, req_body: Json<f64>) -> impl Responder {
     let mut tempo = tempo.write().await;
     *tempo = *req_body;
-    // } else {
-    //     error!("faling to set tempo bc failed to get write lock.");
-    // }
 
     HttpResponse::Ok()
 }
 
 #[get("tempo")]
-async fn get_tempo(
-    tempo: web::Data<RwLock<f64>>,
-    // midi_out: web::Data<MidiOut>,
-    // req_body: Json<f64>,
-) -> impl Responder {
-    // if let Ok(mut tempo) = tempo.write() {
+async fn get_tempo(tempo: web::Data<RwLock<f64>>) -> impl Responder {
     let tempo = tempo.read().await;
-    // *tempo = *req_body;
-    // } else {
-    //     error!("faling to set tempo bc failed to get write lock.");
-    // }
 
     serde_json::to_string(&*tempo).map(|tempo| HttpResponse::Ok().body(tempo))
 }
 
-// #[actix::main]
 pub async fn run(tempo: RwLock<f64>, midi_out: MidiOut) -> std::io::Result<()> {
     let tempo = web::Data::new(tempo);
     let midi_out = web::Data::new(midi_out);
@@ -83,10 +61,7 @@ pub async fn run(tempo: RwLock<f64>, midi_out: MidiOut) -> std::io::Result<()> {
         App::new()
             .app_data(tempo.clone())
             .app_data(midi_out.clone())
-            // .service(hello)
-            // .service(echo)
             // .route("/hey", web::get().to(manual_hello))
-            // .service(note::note)
             .service(midi)
             .service(get_tempo)
             .service(set_tempo)
