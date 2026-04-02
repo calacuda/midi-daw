f5294d88c8f4bd7ce20a2b0b92f183690ddb39f0
Eoghan West <eowest@gmail.com>
2026-04-01 20:05:49 -0400

    started lua env

--- midi-daw-lua/src/main.rs
+++ midi-daw-lua/src/main.rs
@@ -1,3 +1,40 @@
+use mlua::prelude::*;
+
+/// plays a lua function on a device
+fn play_on(
+    lua: Lua,
+    midi_dev: String,
+    channel: Option<i8>,
+    loop_n: Option<isize>,
+    block: Option<bool>,
+) {
+    // store old globals table
+    // set globals table
+    // call function (maybe in a loop)
+    // restore old globas table
+}
+
+/// plays a note
+fn play() {}
+
+/// returns a string of device names
+fn get_devs() -> Vec<String> {
+    return Vec::new();
+}
+
+/// fuzzy finds a device from the list of devices
+fn find_dev(query: &str) -> String {
+    let devs = get_devs();
+
+    devs.get(0).unwrap_or(&String::new()).clone()
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lua = Lua::new();
+    let globals = lua.globals();
+
+    // println!("Hello, world!");
+    if let Err(e) = lua.load(r#"print("hello world from lua!")"#).exec() {
+        eprintln!("lua failed: {e}");
+    }
 }
